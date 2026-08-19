59b3aa6f7fc85a3480b0bfcc34feacc8becc48f0
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-18 22:19:24 -0500

    Punto 3 ¿Terminado?

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        SistemaBiblioteca biblioteca = new SistemaBiblioteca();
 
+        biblioteca.registrarMaterial(new Revista("R-050", "El papel dual de los insectos", 1999, true, 20, "Ciencia"));
+        biblioteca.registrarMaterial(new Libro("L-028", "Los juegos del hambre", 2001,false, "Penelope Saw", "si"));
+
+        Material resultado = biblioteca.buscarMaterial("L-028");
+        resultado.mostrarInfo();
+
+        Material resultado2 = biblioteca.buscarMaterial("L-100");
+        if (resultado2 != null) {
+            resultado2.mostrarInfo();
+        }else {
+            System.out.println("No existe el material");
+        }
+
+        biblioteca.mostrarCatalogo();
     }
 }
--- src/SistemaBiblioteca.java
+++ src/SistemaBiblioteca.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SistemaBiblioteca {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Material m1 = new Libro("L-100", "Pablito y el calvito", 1789, true, "pepe lepew", "yes");
         Material m2 = new Revista("R-005", "La vida de los famosos", 2005, false, 01, "Farandula");
 
         m1.mostrarInfo();
         m2.mostrarInfo();
-    }
+    }*/
 
     Map <String, Material> catalogo = new HashMap<>();
     public void registrarMaterial(Material m){
@@ -16,5 +16,11 @@ public class SistemaBiblioteca {
     }
     public  Material buscarMaterial(String codigo){
         return catalogo.get(codigo);
+    }
+    public void mostrarCatalogo(){
+        for (Map.Entry<String, Material> entrada : catalogo.entrySet()){
+            System.out.println(entrada.getKey());
+            entrada.getValue().mostrarInfo();
+        }
     }
 }
